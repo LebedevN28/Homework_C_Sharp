3e57197bfba09b1866c62914e6a2a4746a98c45c
Nikita Lebedev <lebedevn28@yandex.ru>
2023-02-06 18:24:12 +0300

    Add Homework_003

---
+++ Homework_003/Example019/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome(14212)); // Нет
+        Console.WriteLine(IsPalindrome(12821)); // Да
+        Console.WriteLine(IsPalindrome(23432)); // Да
+    }
+
+    static bool IsPalindrome(int num)
+    {
+        string numString = num.ToString();
+        int length = numString.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            if (numString[i] != numString[length - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Homework_003/Example021/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты точки A: ");
+double Xa = Convert.ToDouble(Console.ReadLine()!);
+double Ya = Convert.ToDouble(Console.ReadLine()!);
+double Za = Convert.ToDouble(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты точки B: ");
+double Xb = Convert.ToDouble(Console.ReadLine()!);
+double Yb = Convert.ToDouble(Console.ReadLine()!);
+double Zb = Convert.ToDouble(Console.ReadLine()!);
+
+double len = Math.Sqrt((Xb - Xa)*(Xb - Xa) + (Yb - Ya)*(Yb - Ya) + (Zb - Za)*(Zb - Za));
+
+Console.WriteLine($"{len:f2}");
