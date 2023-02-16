8eb87819afe277c37945972f59b40ca9c7b39100
Nikita Lebedev <lebedevn28@yandex.ru>
2023-02-16 19:24:26 +0300

    Add example034, 036, 038

---
+++ Homework_005/Example034/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+
+// Создание и заполнение массива
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+// Количество четных элементов
+int CountEven(int[] mas)
+{
+    int count = 0;
+    for(int i = 0; i < mas.Length; i++)
+    {
+        if(mas[i]%2==0) count++;
+    }
+    return count;
+}
+
+int[] array = GetArray(5, 100, 999);
+Console.WriteLine($"В массиве [{string.Join(",", array)}], чётных элементов: {CountEven(array)}");
---
+++ Homework_005/Example038/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+// Создание и заполнение массива
+double[] GetArray(int size)
+{
+    double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = Math.Round(new Random().NextDouble() * 100, 0);
+    }
+    return result;
+}
+
+void Max_Min(double[] mas)
+{
+    double max = mas[0];
+    double min = mas[0];
+    for(int i = 1; i<mas.Length; i++)
+    {
+        if(max<mas[i]) max = mas[i];
+        if(min>mas[i]) min = mas[i];
+    }
+    Console.Write($"Разница между MAX и MIN = {max-min}\n");
+}
+
+
+double[] array = GetArray(4);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Max_Min(array);
