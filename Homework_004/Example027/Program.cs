3b6a7cdf189e1d9b7631402be2edcd56694ccda4
Nikita Lebedev <lebedevn28@yandex.ru>
2023-02-16 19:10:20 +0300

    Add Example034

---
+++ Homework_004/Example027/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int SumNum(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result+=num%10;
+        num=num/10;
+    }
+    return result;
+}
+
+Console.Write($"Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа {a} = {SumNum(a)}");
---
+++ Homework_004/Example029/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+// static void Main(string[] args)
+// {
+//     int[] numbers = new int[8];
+
+//     for (int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = i + 1;
+//     }
+
+//     Console.WriteLine("Elements of the array are: ");
+//     foreach (int i in numbers)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     Console.ReadLine();
+// }
+
+
+int[] Mass(int n)
+{
+    int[] mas = new int[n];
+    for(int i = 0; i<n; i++)
+    {
+        mas[i] = int.Parse(Console.ReadLine()!); 
+    }
+    return mas;
+}
+
+void PrintMas(int[] masiv)
+{
+    for(int i = 0; i<masiv.Length; i++)
+    {
+        Console.Write($"{masiv[i]} ");
+    }
+}
+
+Console.Write("Введите длину массива: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите элементы массива, каждый с новой строчки: ");
+PrintMas(Mass(a));
+
+
+// Ввод массива одной строкой в формате: "12,1,53,64"
+// int[] Mass(string s)
+// {
+//     int cNum = 1;
+//     string res = "";
+//     int lenS = s.Length;
+//     for (int i = 0; i < lenS; i++)
+//     {   
+//         if (s[i].ToString() == ",") cNum+=1;
+//     }
+//     int[] result = new int[cNum];
+//     int index = 0;
+//     for (int i = 0; i < lenS; i++)
+//     {   
+//         if(i != (lenS-1))
+//         {
+//             if (s[i].ToString() != ",") res += s[i];
+//             else
+//             {
+//             result[index] = int.Parse(res);
+//             index++;
+//             res = "";
+//             }
+//         }
+//         else
+//         {
+//             res += s[i];
+//             result[index] = int.Parse(res);
+//             index++;
+//             res = "";
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintMas(int[] mas)
+// {
+//     for (int i = 0; i < mas.Length; i++)
+//     {
+//         Console.Write($"{mas[i]} ");
+//     }
+// }
+
+// Console.Write("Введите числа для создания массива в формате '1,2,3,4' :");
+// string l = Console.ReadLine()!;
+
+// PrintMas(Mass(l));
