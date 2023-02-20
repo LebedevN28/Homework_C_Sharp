8f39fcd7b00136cfe3236c00859f5d29cc8c1717
Nikita Lebedev <lebedevn28@yandex.ru>
2023-02-20 19:49:40 +0300

    Add example041, example 043

---
+++ Homework_006/Example041/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+int CountPositive(int[] mas)
+{
+    int result = 0;
+    for(int i = 0; i<mas.Length; i++) if(mas[i]>0) result++;
+    return result;
+}
+
+Console.Write("Введите числа через пробел:\n");
+int[] array = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();;
+Console.WriteLine($"Количество чисел > 0: {CountPositive(array)}");
